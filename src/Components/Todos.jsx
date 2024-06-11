3b74f035b7ba7a34fca9c76fe286e49b0b36b85f
CodewithM-A <riyazma255@gmail.com>
2024-06-11 10:37:08 +0530

    ....

---
+++ src/Components/Todos.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Todos = () => {
+  return (
+    <div>
+        <div className="container min-w-[80%] border-2 m-auto min-h-[400px]">
+            <h2 className=' text-2xl font-bold text-center'>Todos App</h2>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Todos
